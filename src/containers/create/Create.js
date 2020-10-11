a026193fe2eb73609286c18b2920f428c0d4f9b0
unknown <baybilon_iqbal@hotmail.com>
2020-10-11 19:42:32 +0700

    add some icon and button

--- src/containers/create/Create.js
+++ src/containers/create/Create.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import FormControl from 'react-bootstrap/esm/FormControl';
 import './Create.scss';
@@ -13,17 +13,16 @@ export default function Create() {
           <Link to="/">
             <div className="d-flex justify-content-start align-items-center">
               <img src="/images/icons/arrow-back.svg" alt="arrow-back-icon" />
-              <p className="fs-14 mb-0 mgl-20 header">Tambah Catatan</p>
+              <p className="fs-14 font-weight-bold mb-0 mgl-20 header">Tambah Catatan</p>
             </div>
           </Link>
           <div className="form-group">
             <table border="0" width="100%">
               <tr>
                 <td colSpan="2">
-                  <p className="fs-13 mb-0 mgl-20 header">Jumlah</p>
+                  <p className="fs-12 mb-0 mgl-10 header">Jumlah</p>
                 </td>
               </tr>
-
               <tr>
                 <td>
                   <FormControl className="form-rounded" placeholder="100.000" />
@@ -32,17 +31,15 @@ export default function Create() {
                     {' '}
                     <img src="/images/icons/indonesian-flag.svg" alt="flag-icons" />
                   </Button>
-
                 </td>
-
               </tr>
             </table>
           </div>
         </div>
-        <div align="center " className="btn-frame" size="sm">
-          <Button variant="btn btn-rounded active" size="sm">Pemasukan</Button>
-          <Button variant="btn btn-rounded active" size="sm">Pengeluaran</Button>
-          <Button variant="btn btn-rounded active" size="sm">Transfer</Button>
+        <div align="center " className="btn-frame">
+          <Button variant="btn btn-rounded active">Pemasukan</Button>
+          <Button variant="btn btn-rounded active">Pengeluaran</Button>
+          <Button variant="btn btn-rounded active">Transfer</Button>
         </div>
         <div className="form-group">
           <table border="0" align="center" width="90%">
@@ -50,7 +47,7 @@ export default function Create() {
               <tr>
 
                 <td colSpan="2">
-                  <FormControl className="form-rounded" as="select" defaultValue="Akun">
+                  <FormControl className="form-rounded mgb-5" as="select" defaultValue="Akun">
                     <option disabled>Akun</option>
                     <option>Opo</option>
                     <option>Gojex</option>
@@ -61,14 +58,14 @@ export default function Create() {
               </tr>
               <tr>
                 <td>
-                  <Form>
-                    <FormControl className="form-rounded" type="text" placeholder="Tanggal" />
-                  </Form>
+
+                  <FormControl className="form-rounded" type="text" placeholder="Tanggal" />
+
                 </td>
                 <td>
-                  <Form>
-                    <FormControl className="form-rounded" type="text" placeholder="Waktu" />
-                  </Form>
+
+                  <FormControl className="form-rounded" type="text" placeholder="Waktu" />
+
                 </td>
               </tr>
               <tr>
@@ -78,14 +75,14 @@ export default function Create() {
                     <option>Makan</option>
                     <option>Minum</option>
                   </FormControl>
-                  <Link to="/" className="add"><img src="/images/icons/add-white.svg" alt="add-icon" /></Link>
+                  <div><Link to="/" className="add"><img src="/images/icons/add-white.svg" alt="add-icon" /></Link></div>
                 </td>
               </tr>
               <tr>
                 <td colSpan="2">
-                  <Form>
-                    <FormControl className="form-rounded" as="textarea" placeholder="Catatan" />
-                  </Form>
+
+                  <FormControl className="form-rounded" as="textarea" placeholder="Catatan" />
+
                 </td>
               </tr>
               <td colSpan="2" align="center"><Button className="custom-button btn-color">Simpan</Button></td>
